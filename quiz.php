0b730fbc6b1eec100c57c2b9b08787dbe3660167
suellennn <suellenbarbosa.c@hotmail.com>
2018-06-08 11:40:13 -0300

    perguntas

---
+++ quiz.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+<head>
+	<title>Quiz</title>
+	<style>
+		p{
+			color: black;
+			background: white;
+			text-align: left;
+	}
+
+	</style>
+</head>
+<body>
+	<form method="post" action="ver_resultado.php">
+	<h1>Quiz "Segurança do Trabalho"</h1>
+	<p>
+		<label>1-Quem são os profissionais em Segurança do Trabalho?</label><br/>
+		<input type="radio" name="pergunta1" value="opção1" />Técnico de Segurança do Trabalho, Engenheiro de Segurança do Trabalho, Médico do Trabalho e Enfermeiro do Trabalho.<br/>
+		<input type="radio" name="pergunta1" value="opção2" />Técnico de Segurança do Trabalho, Engenheiro Químico, Médico do Trabalho e Enfermeiro do Trabalho.<br/>
+		<input type="radio" name="pergunta1" value="opção3" />CAT, Engenheiro de Segurança do Trabalho, Médico do Trabalho e Enfermeiro do Trabalho.<br/>
+		<input type="radio" name="pergunta1" value="opção4" />Médico Ortopedista, CAT, Engenheiro Químico, Médico do Trabalho.<br/>
+	</p>
+
+	<p>
+		<label>2-O que significa CAT?</label><br/>
+		<input type="radio" name="pergunta2" value="opção1" />Comunicado de Acidente do Trabalho.<br/>
+		<input type="radio" name="pergunta2" value="opção2" />Carteira Automática de Trabalho.<br/>
+		<input type="radio" name="pergunta2" value="opção3" />Comunicação de Acidente do Trabalho.<br/>
+		<input type="radio" name="pergunta2" value="opção4" />Carta de Acidente de Trabalho.<br/>
+	</p>
+
+	<p>
+		<label>3-O que é CIPA?</label><br/>
+		<input type="radio" name="pergunta3" value="opção1" />Comissão Interna de Prevenção Agrária.<br/>
+		<input type="radio" name="pergunta3" value="opção2" />Comissão Interna de Prevenção de Acidentes.<br/>
+		<input type="radio" name="pergunta3" value="opção3" />Contribuição Interna Para Prevenção de Acidentes.<br/>
+		<input type="radio" name="pergunta3" value="opção4" />Nenhuma das Alternativas.<br/>
+	</p>
+
+	<p>
+		<label>4-Quais os Tipos de Acidente de Trabalho?</label><br/>
+		<input type="radio" name="pergunta4" value="opção1" />Típico, Atípico e Rotineiro.<br/>
+		<input type="radio" name="pergunta4" value="opção2" />Típico, De trajeto e Atípico.<br/>
+		<input type="radio" name="pergunta4" value="opção3" />Atípico, Fora do Trajeto, Típico.<br/>
+		<input type="radio" name="pergunta4" value="opção4" />De Trajeto, Tropical e Atípico.<br/>
+	</p>
+
+	<p>
+		<label>5-Qual dos profissionais a seguir não faz parte do SESMT?</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />Enfermeiro do Trabalho.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />Fisioterapeuta do Trabalho.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />Auxiliar de Enfermagem do Trabalho.<br/>
+		<input type="radio" name="pergunta5" value="opção4" />Engenheiro de Segurança do Trabalho.<br/>
+	</p>
+
+	<p>
+		<label>6-Qual a definição correta de LER.</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />Lesão por Estudo Repetitivo.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />legalização do Esforço Repetitivo.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />Lesão por Esforço Repetitivo.<br/>
+		<input type="radio" name="pergunta5" value="opção4" />Lei do Estudo repetitivo.<br/>
+	</p>
+
+	<p>
+		<label>7-Quais as cores do Mapa de Riscos de Acidentes do Trabalho?</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />Verde, Vermelho, Marrom, Amarelo e Azul.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />Azul, Roxo, Verde, Vermelho, Lilás.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />Vermelho, verde, Preto e Azul.<br/>
+		<input type="radio" name="pergunta5" value="opção4" />Verde, Vermelho, Cinza, Amarelo e Azul.<br/>
+	</p>
+
+	<p>
+		<label>8-Qual a definição de EPI.</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />Equipamento de proteção ímpar.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />Eixo de Proteção Individual.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />Equipamento Peculiar Individual..<br/>
+		<input type="radio" name="pergunta5" value="opção4" />Equipamento de Proteção Individual..<br/>
+	</p>
+
+	<p>
+		<label>9-Do que se trata a NR-5?</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />PPRA.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />PCMSO.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />AET.<br/>
+		<input type="radio" name="pergunta5" value="opção4" />CIPA.<br/>
+	</p>
+
+	<p>
+		<label>10-O que é CLT?</label><br/>
+		<input type="radio" name="pergunta5" value="opção1" />Consolidação Legal do Trabalho.<br/>
+		<input type="radio" name="pergunta5" value="opção2" />Consolidação das Leis do Trabalho.<br/>
+		<input type="radio" name="pergunta5" value="opção3" />Confederação das Leis Trabalhistas.<br/>
+		<input type="radio" name="pergunta5" value="opção4" />Consolidação Legal do Trabalho.<br/>
+	</p>
+
+	</form>
+<input type="submit" name="ver resultado">
+
+</body>
+</html>
+
